e9a6e2ed1c486bb51f20f6b75157cd7b993445c9
rubikoy97 <45144048+rubikoy97@users.noreply.github.com>
2018-12-15 12:44:16 +0100

    Create Server.java

---
+++ Server/pds/Server.java
@@ -0,0 +1,19 @@
+package pds;
+
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private ServerSocket socketserver; // le server
+    private BufferedReader in ; // pour recevoir le message du client
+    private PrintWriter out ; // pour envoyer le message au client
+    private int ServerPort = 9999; // port utilisé
+
+    private Socket client; // Socket du client
+
+    private String messageReceived = "";
+
+    private String response = "";
