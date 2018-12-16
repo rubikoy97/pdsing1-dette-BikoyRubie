cee1d72db090cb864e2a9c756ef40e11aab1fa77
rubikoy97 <45144048+rubikoy97@users.noreply.github.com>
2018-12-16 12:12:49 +0100

    Update Server.java

--- Server/pds/Server.java
+++ Server/pds/Server.java
@@ -17,3 +17,43 @@ public class Server {
     private String messageReceived = "";
 
     private String response = "";
+
+    public Server() throws IOException {
+        socketserver = new ServerSocket(ServerPort);
+    }
+
+
+    public void AcceptConnection(){
+        try {
+
+            client = this.socketserver.accept();
+
+
+            out = new PrintWriter(new OutputStreamWriter(client.getOutputStream()),true) ;
+
+            in = new BufferedReader(new InputStreamReader(client.getInputStream())) ;
+
+
+                while (!client.isClosed()) {
+
+                    messageReceived = in.readLine();
+
+                    if(messageReceived.equals(null)) client.close();
+
+                    switch (messageReceived) {
+                        // le server appelle la base de données avec un SELECT
+                        case "netflix":
+
+                            response = "vous avez envoyé NETFLIX";
+                            break;
+
+                    }
+                    // On retourne la réponse au client
+                    out.println(response);
+                    response = "";
+                }
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.err.println("<!> end of stream <!>");
+        }
+    }
