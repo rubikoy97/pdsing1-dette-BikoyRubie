73048e0afa8cd9028e6aae90d7853f24fe11f540
rubikoy97 <45144048+rubikoy97@users.noreply.github.com>
2019-05-12 16:23:52 +0200

    Create GUI.java

---
+++ Client/pds/GUI.java
@@ -0,0 +1,17 @@
+package pds;
+
+import javax.swing.JFrame;
+
+public class GUI extends JFrame {
+
+    private static final long serialVersionUID = 1619230176443382571L;
+
+    public GUI() {
+
+        this.setSize(1200, 720);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+}
