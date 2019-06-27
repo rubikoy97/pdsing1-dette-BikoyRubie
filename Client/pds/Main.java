649c616d82364c533a51d461ed28133aa0de987c
rubikoy97 <45144048+rubikoy97@users.noreply.github.com>
2019-06-27 20:49:18 +0200

    Update Main.java

--- Client/pds/Main.java
+++ Client/pds/Main.java
@@ -2,7 +2,7 @@ package pds;
 
 public class Main {
     public static void main(String[] args) {
-        Thread t = new Thread(new Client("localhost",9999));
+        Thread t = new Thread(new Client("192.168.20.4",9999));
         t.start();
     }
 }
